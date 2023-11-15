1175902e2e2959383805a0e9dcc6b5252109cdfa
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-15 17:32:14 +0100

    fix: esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
 /* funzione riutilizzabile */
-function toggleProperty(elementClass, property) {
-  let element = document.querySelector(elementClass);
-  if (element) {
-    element.classList.toggle(property);
-  }
-  return element;
+function toggleProperty(element, property) {
+  element.classList.toggle(property);
 }
+
 /* bottone che scompare */
+const boxAdder = document.querySelector(".board");
 const buttonToggle = document.querySelector(".play");
+
 buttonToggle.addEventListener("click", function () {
-  toggleProperty(".board", "hidden");
+  toggleProperty(boxAdder, "hidden");
 });
 
-const boxAdder = document.querySelector(".board");
-
+/* casella diventa rossa al click */
 for (let i = 0; i < 100; i++) {
   const boxes = document.createElement("div");
   boxes.classList.add("box");
   boxes.classList.add("flex");
   boxes.innerHTML = i + 1;
   boxAdder.append(boxes);
+  boxes.addEventListener("click", function () {
+    toggleProperty(boxes, "color");
+    console.log("hai cliccato sulla casella.");
+  });
 }
-
-/* casella diventa rossa al click */
-const boxClicker = document.querySelectorAll(".box");
-boxClicker.addEventListener("click", function () {
-  toggleProperty(".box", ".color");
-});
